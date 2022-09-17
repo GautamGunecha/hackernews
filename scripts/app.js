6039e4e475faa1090a12968812c328f2f3b6a1db
Gautam Gunecha <gautamgunecha@gmail.com>
2022-09-17 16:15:54 +0530

    clousers

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,7 @@
 const BASE_URL = "https://api.hnpwa.com/v0";
 let loading = false;
+let currentPage = 0;
+let rows_per_page = 30;
 
 // To avoid too many if-else using object point reff
 const URL_END_POINT = {
@@ -20,8 +22,9 @@ const getCurrentURL = () => {
 
     const product = urlParams.get("search");
     const val = returnURLVAL(product);
+    currentPage += 1;
 
-    fetchAPI(val, 1);
+    fetchAPI(val, currentPage);
   } catch (error) {
     console.log(error);
   }
@@ -34,56 +37,78 @@ const returnURLVAL = (product) => {
 
 const fetchAPI = async (URL, PAGE) => {
   loading = true;
+  let newsData = [];
   try {
     const response = await axios.get(`${BASE_URL}/${URL}/${PAGE}.json`);
     loading = false;
-    displayAllNews(response.data);
+
+    if (!response.data) {
+      loading = false;
+      let section = document.createElement("section");
+
+      let p = document.createElement("p");
+      p.innerText = "End Page";
+
+      section.appendChild(p);
+      document.querySelector(".content-news").appendChild(section);
+    }
+
+    newsData.push(response.data);
+
+    const displayAllNews = (newsData) => {
+      newsData.forEach((news) => {
+        if (loading) {
+          let section = document.createElement("section");
+
+          let p = document.createElement("p");
+          p.innerText = "Loading...";
+
+          section.appendChild(p);
+          document.querySelector(".content-news").appendChild(section);
+        } else {
+          let section = document.createElement("section");
+          let h1 = document.createElement("h1");
+          h1.innerText = news.title;
+
+          let span = document.createElement("span");
+          let userSpan = document.createElement("span");
+          userSpan.setAttribute("class", "user");
+          let dateSpan = document.createElement("span");
+
+          let p = document.createElement("p");
+          if (news.points || news.user) {
+            p.innerText = `${news.points} by `;
+            userSpan.innerText = `${news.user} `;
+            dateSpan.innerText = `| ${news.time_ago}`;
+
+            p.appendChild(userSpan);
+            p.appendChild(dateSpan);
+          } else {
+            p.innerText = ` ${news.time_ago}`;
+          }
+          h1.appendChild(span);
+
+          let anchor = document.createElement("a");
+          anchor.setAttribute("href", news.url);
+          anchor.setAttribute("target", "__blank");
+          anchor.innerText = news.domain;
+
+          span.append(anchor);
+
+          section.appendChild(h1);
+          section.appendChild(p);
+
+          document.querySelector(".content-news").appendChild(section);
+        }
+      });
+    };
+
+    return displayAllNews(newsData[0]);
   } catch (error) {
     console.log(error);
     loading = false;
+    newsData = [];
   }
-};
-
-// Example
-
-const displayAllNews = (newsData) => {
-  newsData.forEach((news) => {
-    if (loading) {
-      let section = document.createElement("section");
-
-      let p = document.createElement("p");
-      p.innerText = "Loading...";
-
-      section.appendChild(p);
-      document.querySelector(".content-news").appendChild(section);
-    } else {
-      let section = document.createElement("section");
-      let h1 = document.createElement("h1");
-      h1.innerText = news.title;
-
-      let span = document.createElement("span");
-
-      let p = document.createElement("p");
-      if (news.points || news.user) {
-        p.innerText = `${news.points} by ${news.user} | ${news.time_ago}`;
-      } else {
-        p.innerText = ` ${news.time_ago}`;
-      }
-      h1.appendChild(span);
-
-      let anchor = document.createElement("a");
-      anchor.setAttribute("href", news.url);
-      anchor.setAttribute("target", "__blank");
-      anchor.innerText = news.domain;
-
-      span.append(anchor);
-
-      section.appendChild(h1);
-      section.appendChild(p);
-
-      document.querySelector(".content-news").appendChild(section);
-    }
-  });
 };
 
 getCurrentURL();
